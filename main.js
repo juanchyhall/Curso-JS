e25719a074fcd91c8728d0dcf3be8611c90ecba7
Juan Manuel Hall <juanchyhall@gmail.com>
2021-09-27 21:24:02 -0300

    Primera entrega

---
+++ main.js
@@ -0,0 +1,7 @@
+const nombre = prompt("Bienvenido! Por favor, ingrese su nombre")
+
+let valorTomate = 5
+
+let valorCompraTomates = 5 * parseInt(prompt("Ingrese la cantidad de tomates que desee comprar"))
+
+alert("Hola "+nombre+"! El monto total por tu compra es de $"+valorCompraTomates)

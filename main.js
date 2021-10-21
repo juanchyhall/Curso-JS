3cb919c538b9b7a98046e5eca4d2a58986b0a121
Juan Manuel Hall <juanchyhall@gmail.com>
2021-10-21 00:44:15 -0300

    Agrega listado con ordenamiento

--- main.js
+++ main.js
@@ -324,6 +324,7 @@ do {
 
 /*-------------------------  Sexta Entrega-----------------*/
 
+
 class Reserva{
     constructor(nombre,apellido,dia, hora ){
         this.nombre = nombre
@@ -331,7 +332,6 @@ class Reserva{
         this.dia = dia
         this.hora = hora
     }
-
 }
 let nombreReserva = ""
 let apellidoReserva = ""
@@ -345,25 +345,51 @@ function consultarDatosReserva(){
     horaReserva = parseInt(prompt("Ingrese la hora de la reserva"))
 }
 function crearReserva() {
-    const reserva = new Reserva(nombreReserva,apellidoReserva, diaReserva,horaReserva)
+     const reserva = new Reserva(nombreReserva,apellidoReserva, diaReserva,horaReserva)
     
     listadoReservas.push(reserva)
 
+   
+   
 }
 function listarReservas(){
+    console.log("Listado de reservas")
     for(let i = 0;i<listadoReservas.length;i++){
-
+        
         console.log(listadoReservas[i])
+        
     }
 }
+alert("Ingrese los datos para una nueva reserva")
 function ejecutarReserva(){
     do{
-        alert("Ingrese los datos para una nueva reserva")
         consultarDatosReserva()
         crearReserva()
         
         nuevaReserva = confirm("Desea realizar otra reserva?")
     }while(nuevaReserva)
 }
+/*-------------------------Con desafio complementario-------------*/
+function ordenarPorHora() {
+    const listadoOrdenado = listadoReservas.sort((a,b) =>{
+        return b.hora + a.hora 
+    })
+    console.log("Listado por hora, de menor a mayor")
+    console.log(listadoOrdenado)
+}
+function ordenarPorNombre() {
+    const listadoOrdenado =listadoReservas.sort((a,b) =>{
+        if(a.nombre> b.nombre){
+            return 1
+        }else{
+            return -1
+        }
+    })
+    console.log("Listado por nombre de la A a la Z")
+    console.log(listadoOrdenado)
+}
 ejecutarReserva()
 listarReservas()
+ordenarPorHora()
+ordenarPorNombre()
+
